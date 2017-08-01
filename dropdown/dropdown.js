d959b92496fa7270ef8b86e1e7517dcdce8d8eb9
Zander Maxwell <awezmaz@hotmail.com>
2017-08-01 16:07:46 -0700

    Properly hid dropdown
    When clicking anything but the dropdown, the dropdown is hidden.

--- dropdown/dropdown.js
+++ dropdown/dropdown.js
@@ -1,17 +1,28 @@
-// On load, toggle the class to hide the dorpdown
-function myFunction() {
-    document.getElementById('myDropdown').classList.toggle('show');
-}
-// When some element is clicked:
-//  if it is not the dropdown button, hide the dropdown content
-window.onclick = function(event) {
-    if (!event.target.matches('dropbtn')) {
-        var dropdowns=getElementsByClassName('dropdown-content');
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdowns.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+(function(window, document, undefined) {
+    window.onload = init;
+
+    // Code to be called when the dom has loaded
+    //  i.e. #document has its nodes
+    function init() {
+        var dropButton = document.getElementById('dropdownContainer');
+
+        document.addEventListener('click', function(event) {
+            var clickedInside = dropButton.contains(event.target);
+
+            // When some element is clicked:
+            //  if it is not the dropdown button, hide the dropdown content
+            if (!clickedInside) {
+                var dropdowns = document.getElementsByClassName('dropdownContent');
+                for (var i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            } else {
+                document.getElementById('dropdownLinks').classList.toggle('show');
             }
-        }
+        });
     }
-}
+
+})(window, document, undefined);
